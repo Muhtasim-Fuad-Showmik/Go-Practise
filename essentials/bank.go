d9533980424b5758785675080452451dce860c92
Muhtasim-ZoloInc <muhtasim.zoloinc@gmail.com>
2026-09-06 10:49:16 +0600

    refactor: Extracted menu display logic into presentOptions function in separate communication file

--- essentials/bank.go
+++ essentials/bank.go
@@ -22,12 +22,7 @@ func initBanking() {
 
 MainLoop:
 	for {
-		// Output operations
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit")
-		fmt.Println("3. Withdraw")
-		fmt.Println("4. Exit")
+		presentOptions()
 
 		// Input operations
 		var choice int
