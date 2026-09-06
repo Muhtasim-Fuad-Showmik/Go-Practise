c98e066b05be77d2a22a520f31f7416b51db56c0
Muhtasim-ZoloInc <muhtasim.zoloinc@gmail.com>
2026-09-06 11:20:15 +0600

    feat: Added random phone number generation to menu using go-randomdata package

--- essentials/communication.go
+++ essentials/communication.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Pallinder/go-randomdata"
+)
 
 func presentOptions() {
 	fmt.Println("What do you want to do?")
+	fmt.Println("Reach us 24/7 at", randomdata.PhoneNumber())
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit")
 	fmt.Println("3. Withdraw")
--- essentials/go.mod
+++ essentials/go.mod
@@ -1,3 +1,5 @@
-module example.com/investment-calculator
+module example.com/essentials
 
 go 1.27.0
+
+require github.com/Pallinder/go-randomdata v1.2.0 // indirect
