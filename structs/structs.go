d48579e84a7da20500ce9f31e1d24e53f6e451ac
Muhtasim-ZoloInc <muhtasim.zoloinc@gmail.com>
2026-09-06 12:49:14 +0600

    feat: Added user struct example with data collection and initialization using time.Now()

---
+++ structs/structs.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	fmt.Println(firstName, lastName, birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
