1ddae3e8eb735e22078ae4c7a73ee9a8754aef3e
Muhtasim-ZoloInc <muhtasim.zoloinc@gmail.com>
2026-09-15 11:53:26 +0600

    feat: Added array operations including initialization, indexing, and slicing with example outputs

--- lists/main.go
+++ lists/main.go
@@ -9,6 +9,22 @@ type Product struct {
 }
 
 func main() {
+	// Array Definitions
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	var productNames [4]string = [4]string{"A Book"}
+
+	// Array value assignment at an index
+	productNames[2] = "A Carpet"
+
+	// Output entire arrays
 	fmt.Println(prices)
+	fmt.Println(productNames)
+
+	// Output first value of the array
+	fmt.Println(prices[0])
+
+	// Slicing an array
+	featuredPrices := prices[1:3]
+
+	fmt.Println(featuredPrices)
 }
