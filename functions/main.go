e9f88dcef764a068b3a8347f6e715e3a861086d4
Muhtasim-ZoloInc <muhtasim.zoloinc@gmail.com>
2026-09-21 17:54:18 +0600

    understand closures and factories

--- functions/main.go
+++ functions/main.go
@@ -9,11 +9,22 @@ type transformFn func(int) int
 func main() {
 	numbers := []int{1, 2, 3, 4}
 
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
 	transformedNumbers := transformNumbers(&numbers, func(number int) int {
 		return number * 2
 	})
 
 	fmt.Println(transformedNumbers)
+
+	// Closeure: Closes created function via variable passed into factory method
+	// Generated functions remain unaffected by multiple calls of the factory functions
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
 }
 
 // Passing functions as values into the parameters for another function
@@ -24,4 +35,11 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return tNumbers
+}
+
+// Factory function for producing transformer functions
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
