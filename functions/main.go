722be5329cbeaa987efe024e0c5da8ba4bdbe956
Muhtasim-ZoloInc <muhtasim.zoloinc@gmail.com>
2026-09-23 14:26:07 +0600

    slice parameters to values

--- functions/main.go
+++ functions/main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	sum := sumUp(1, 10, 15)
+	numbers := []int{1, 10, 15, 40}
+	anotherSum := sumUp(numbers...)
 
 	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
 // Variadic Function: A function where the number of parameters is dynamic and therefore can vary
